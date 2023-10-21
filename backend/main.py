514324fac961d52d4288b7ef100b87717806bddc
Rishika Nayak <nayak.rishikad@gmail.com>
2023-10-21 15:13:25 -0400

    railway config

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,4 @@ from app.api import app
 
 #To Run: uvicorn main:app --reload
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
